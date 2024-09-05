0ff2893b87eb990a1e3057dc513ca19baa560e9f
Ricardo <ri.tapiasaez@gmail.com>
2024-09-05 16:55:00 -0400

    Child-to-Parent Communication

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
+import { useState } from "react";
+
 import Form from "./modules/Form";
 import Logo from "./modules/Logo";
 import PackingList from "./modules/PackingList";
 import Stats from "./modules/Stats";
 
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} onDeleteItem={handleDeleteItem} />
       <Stats />
     </div>
   );
